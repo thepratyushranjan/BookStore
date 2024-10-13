28b07d61d30af457d985f72d6aa042d0c70dc744
thepratyushranjan <thepratyushranjan6060@gmail.com>
2024-10-13 19:42:50 +0530

    change db

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -90,23 +90,23 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-     'ENGINE': 'django.db.backends.mysql', 
-     'NAME': 'books_store', 
-     'USER': 'root', 
-     'PASSWORD': 'root', 
-     'HOST': 'localhost', 
-     'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#      'ENGINE': 'django.db.backends.mysql', 
+#      'NAME': 'books_store', 
+#      'USER': 'root', 
+#      'PASSWORD': 'root', 
+#      'HOST': 'localhost', 
+#      'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
